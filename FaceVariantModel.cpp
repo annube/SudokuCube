e2c2258fc55604fbdb9c3306d07ef7c08ef729f2
Andreas Ammon <nubi@freenet.de>
2024-01-24 22:35:39 +0100

    elaborate FaceVariantModel

--- FaceVariantModel.cpp
+++ FaceVariantModel.cpp
@@ -8,16 +8,33 @@ FaceVariantModel::FaceVariantModel(int face, QObject *parent)
 
 int FaceVariantModel::rowCount(const QModelIndex& parent) const
 {
+  return 4;
 }
 
 QVariant FaceVariantModel::data(const QModelIndex& index, int role) const
 {
+  switch(role)
+  {
+  case Qt::DisplayRole:
+    return QString("Variant %1").arg(index.row());
+  case VariantPossible:
+    return POSSIBLE;
+  case ModelIndex:
+    return index;
+  default:
+    return QVariant();
+  }
 }
 
 QVariant FaceVariantModel::headerData(int section, Qt::Orientation orientation, int role) const
 {
+  return QStringLiteral("Variant");
 }
 
 QHash<int, QByteArray> FaceVariantModel::roleNames() const
 {
+  return {
+    {VariantPossible, "possible"},
+    {ModelIndex, "modelIndex"},
+  };
 }
--- FaceVariantModel.h
+++ FaceVariantModel.h
@@ -3,15 +3,30 @@
 
 #include <QAbstractListModel>
 #include <FaceCoords.h>
+#include <QtQml/qqmlregistration.h>
 
 class FaceVariantModel : public QAbstractListModel
 {
   Q_OBJECT
+  QML_ELEMENT
 public:
   explicit FaceVariantModel(int face, QObject *parent = nullptr);
 
   // QAbstractItemModel interface
 public:
+  enum Roles
+  {
+    VariantPossible = Qt::UserRole+1,
+    ModelIndex,
+  };
+  enum VariantState
+  {
+    POSSIBLE,
+    IMPOSSIBLE,
+    SELECTED,
+  };
+  Q_ENUMS(VariantState);
+
   virtual int rowCount(const QModelIndex& parent) const override;
   virtual QVariant data(const QModelIndex& index, int role) const override;
   virtual QVariant headerData(int section, Qt::Orientation orientation, int role) const override;
