72d2c29e9d77b45ef23e2cc1ba0d4a9ca00fb5b6
Andreas Ammon <nubi@freenet.de>
2024-01-25 21:18:04 +0100

    generalize face variant model (as preparation to split it ..)
    into generic part (VariantModel) and specific part FaceVariantModel

--- FaceVariantModel.cpp
+++ FaceVariantModel.cpp
@@ -1,15 +1,35 @@
 #include "FaceVariantModel.h"
+#include "CubeModel.h"
+#include <set>
 
-FaceVariantModel::FaceVariantModel(int face, QObject *parent)
-  : QAbstractListModel{parent}, _face(face), _faceCoords(face)
+FaceVariantModel::FaceVariantModel(CubeModel* cubeModel, int face, QObject *parent)
+  : QAbstractListModel{parent}, _cubeModel(cubeModel), _face(face), _faceCoords(face)
+  , _centerCoord(_faceCoords.localToGlobalCoord(1,1))
+
 {
 
+  _variantPieces.resize(numVariants());
+  for(int variant = 0; variant < numVariants(); ++variant)
+  {
+    _variantPieces[variant].push_back(_faceCoords.localToGlobalCoord(1,1,0));
+    _variantPieces[variant].push_back(_faceCoords.faceVariantEdgeCoord(variant));
+    _variantPieces[variant].push_back(_faceCoords.faceVariantCornerCoord(variant));
+    std::sort(_variantPieces[variant].begin(), _variantPieces[variant].end());
+  }
+
+  _cubeModel->registerPostChange([this](int x, int y, int z){
+    dataChanged(index(0), index(numVariants()-1));
+  });
+
 }
+
 
 int FaceVariantModel::rowCount(const QModelIndex& parent) const
 {
-  return 4;
+  return numVariants();
 }
+
+
 
 QVariant FaceVariantModel::data(const QModelIndex& index, int role) const
 {
@@ -18,7 +38,31 @@ QVariant FaceVariantModel::data(const QModelIndex& index, int role) const
   case Qt::DisplayRole:
     return QString("Variant %1").arg(index.row());
   case VariantPossible:
-    return POSSIBLE;
+  {
+    auto coordsWithCenterColor = _cubeModel->getCoordsWithValue(color());
+    std::sort(coordsWithCenterColor.begin(), coordsWithCenterColor.end());
+
+    if(coordsWithCenterColor.size()>_variantPieces.size() || !variantFree(index.row()))
+    {
+      return IMPOSSIBLE;
+    }
+
+    if(coordsWithCenterColor == _variantPieces[index.row()])
+    {
+      return SELECTED;
+    }
+
+    std::vector<std::array<int, 3>> commonPieces;
+    std::set_intersection(coordsWithCenterColor.begin(), coordsWithCenterColor.end(), _variantPieces[index.row()].begin(), _variantPieces[index.row()].end(),
+                          std::back_inserter(commonPieces));
+
+    if(commonPieces == coordsWithCenterColor)
+    {
+      return POSSIBLE;
+    }
+    return IMPOSSIBLE;
+
+  }
   case ModelIndex:
     return index;
   default:
@@ -37,4 +81,59 @@ QHash<int, QByteArray> FaceVariantModel::roleNames() const
     {VariantPossible, "possible"},
     {ModelIndex, "modelIndex"},
   };
+}
+
+void FaceVariantModel::setVariant(int variant)
+{
+  if(variantFree(variant))
+  {
+    bool wasSelected = variantSelected(variant);
+    // clear color
+    _cubeModel->clearColor(color());
+
+    if(!wasSelected) {
+      // set new variant
+      for(auto const& piece: _variantPieces[variant])
+      {
+        _cubeModel->setValue(piece, color());
+      }
+    }
+  }
+}
+
+int FaceVariantModel::numVariants() const
+{
+  return 4;
+}
+
+CubeModel::CubeColors FaceVariantModel::color() const
+{
+  return CubeModel::faceCenterColor(_face);
+}
+
+
+bool FaceVariantModel::variantFree(int variant) const
+{
+  for(auto const& piece: _variantPieces[variant])
+  {
+    auto pieceValue = _cubeModel->getValue(piece);
+    if( pieceValue != color() && pieceValue != CubeModel::FREE)
+    {
+      return false;
+    }
+  }
+  return true;
+}
+
+bool FaceVariantModel::variantSelected(int variant) const
+{
+  for(auto const& piece: _variantPieces[variant])
+  {
+    auto pieceValue = _cubeModel->getValue(piece);
+    if( pieceValue != color())
+    {
+      return false;
+    }
+  }
+  return true;
 }
--- FaceVariantModel.h
+++ FaceVariantModel.h
@@ -4,13 +4,15 @@
 #include <QAbstractListModel>
 #include <FaceCoords.h>
 #include <QtQml/qqmlregistration.h>
+#include "CubeModel.h"
 
+class CubeModel;
 class FaceVariantModel : public QAbstractListModel
 {
   Q_OBJECT
   QML_ELEMENT
 public:
-  explicit FaceVariantModel(int face, QObject *parent = nullptr);
+  explicit FaceVariantModel(CubeModel* cubeModel, int face, QObject *parent = nullptr);
 
   // QAbstractItemModel interface
 public:
@@ -31,10 +33,19 @@ public:
   virtual QVariant data(const QModelIndex& index, int role) const override;
   virtual QVariant headerData(int section, Qt::Orientation orientation, int role) const override;
   virtual QHash<int, QByteArray> roleNames() const override;
+  Q_INVOKABLE void setVariant(int variant);
 
+  int numVariants() const;
+  CubeModel::CubeColors color() const;
 private:
+  bool variantFree(int variant) const;
+  bool variantSelected(int variant) const;
+  CubeModel* _cubeModel;
   int _face;
   FaceCoords _faceCoords;
+  std::array<int, 3> _centerCoord;
+  std::vector<std::vector<std::array<int, 3>>> _variantPieces;
+
 };
 
 #endif // FACEVARIANTMODEL_H
